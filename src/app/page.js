22c2fc4efb957bba5a124d8e2701e7e4341fc60a
Shelbe Andrade <shelbe@cloudflare.com>
2023-08-01 19:48:27 +0100

    add functionality 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 export default async function Home() {
-  const json_data = 'trial company'
+  const json_data = await fetch('https://bucket-fetcher.shelbe-ent.workers.dev').then(response => response.json())
+  const companyName = await json_data.companyName
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2">
@@ -9,7 +10,7 @@ export default async function Home() {
                     rel="noopener noreferrer"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        ${json_data}
+                        Company name is {companyName}
                     </h2>
                 </a>
             </div>
